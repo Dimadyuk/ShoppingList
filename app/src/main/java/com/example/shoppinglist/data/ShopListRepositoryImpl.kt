12ac5f00859c575a6bc3ac47301b349921dd298d
Dimadyuk <dimadyuk88@gmail.com>
2022-05-17 16:43:45 +0300

    lesson 29

--- app/src/main/java/com/example/shoppinglist/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/example/shoppinglist/data/ShopListRepositoryImpl.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.shoppinglist.domain.ShopItem
 import com.example.shoppinglist.domain.ShopListRepository
+import kotlin.random.Random
 
 object ShopListRepositoryImpl: ShopListRepository {
     private val shopListLD = MutableLiveData<List<ShopItem>>()
@@ -14,7 +15,7 @@ object ShopListRepositoryImpl: ShopListRepository {
 
     init {
         for (i in 0..1000) {
-            val item = ShopItem("Name $i", i, true)
+            val item = ShopItem("Name $i", i, Random.nextBoolean())
             addShopItem(item)
         }
     }
--- app/src/main/java/com/example/shoppinglist/presentation/ShopListAdapter.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/ShopListAdapter.kt
@@ -1,34 +1,65 @@
 package com.example.shoppinglist.presentation
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.example.shoppinglist.R
 import com.example.shoppinglist.domain.ShopItem
 
 class ShopListAdapter : RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>() {
 
-    val list = listOf<ShopItem>()
+    var count = 0
+    var shopList = listOf<ShopItem>()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShopItemViewHolder {
-        val view =
-            LayoutInflater.from(parent.context).inflate(R.layout.item_shop_enabled, parent, false)
+        Log.d("onCreateViewHolder", "count: ${++count}")
+        val view = LayoutInflater.from(parent.context).inflate(
+            R.layout.item_shop_enabled,
+            parent,
+            false
+        )
         return ShopItemViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: ShopItemViewHolder, position: Int) {
-        val shopItem = list[position]
-        holder.tvName.text = shopItem.name
-        holder.tvCount.text = shopItem.count.toString()
-        holder.view.setOnLongClickListener {
+    override fun onBindViewHolder(viewHolder: ShopItemViewHolder, position: Int) {
+        val shopItem = shopList[position]
+        val status = if (shopItem.enabled){
+            "Active"
+        } else {
+            "not active"
+        }
+
+        viewHolder.view.setOnLongClickListener {
             true
+        }
+        if (shopItem.enabled){
+            viewHolder.tvName.text = "${shopItem.name} $status"
+            viewHolder.tvCount.text = shopItem.count.toString()
+            viewHolder.tvName.setTextColor(ContextCompat.getColor(viewHolder.view.context, android.R.color.holo_red_dark))
+        } else {
+            super.onViewRecycled(viewHolder)
+            viewHolder.tvName.text = "${shopItem.name} $status"
+            viewHolder.tvCount.text = shopItem.count.toString()
+            viewHolder.tvName.setTextColor(ContextCompat.getColor(viewHolder.view.context,
+                R.color.white))
         }
     }
 
+
+//    override fun getItemViewType(position: Int): Int {
+//        return position
+//    }
+
     override fun getItemCount(): Int {
-        return list.size
+        return shopList.size
     }
 
     class ShopItemViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
